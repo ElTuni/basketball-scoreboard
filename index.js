5cfcac56429f9b07ee65dd30896a3e4e8795e973
ElTuni <tomaslucasgoya@gmail.com>
2025-03-16 20:52:07 -0300

    idk

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
-let count_home = document.getElementById("count-home")
-let count_guest = document.getElementById("count-guest")
-
 function plus(where, points) {
     let total = Number(where.textContent) + points
     where.textContent = total
+    
+    let count_home = document.getElementById("count-home")
+    let count_guest = document.getElementById("count-guest")
+    
     if (Number(count_home.textContent) > Number(count_guest.textContent)) {
         count_home.style.boxShadow = "0px 0px 15px white"
         count_guest.style.boxShadow = "0px 0px 0px white"
-    }else if (Number(count_home.textContent) < Number(count_guest.textContent)) {
+    } else if (Number(count_home.textContent) < Number(count_guest.textContent)) {
         count_home.style.boxShadow = "0px 0px 0px white"
         count_guest.style.boxShadow = "0px 0px 15px white"
-    }else {
+    } else {
         count_home.style.boxShadow = "0px 0px 0px white"
         count_guest.style.boxShadow = "0px 0px 0px white"
-    }}
+    }
+}
 
 function reset() {
-   count_home.textContent = 0
-   count_guest.textContent = 0
+    document.getElementById("count-home").textContent = 0
+    document.getElementById("count-guest").textContent = 0
+    document.getElementById("count-home").style.boxShadow = "0px 0px 0px white"
+    document.getElementById("count-guest").style.boxShadow = "0px 0px 0px white"
 }
+
+// Expose functions globally
+window.plus = plus;
+window.reset = reset;
